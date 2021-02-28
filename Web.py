d4f6ffe077e0f1507b5e7049e6e98c3a0fa347c7
sau-skt <saurabhchatterjee18@gmail.com>
2021-02-28 13:26:55 +0530

    Completed

---
+++ Web.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get("https://news.ycombinator.com/news")
+soup = BeautifulSoup(res.text,'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def sort_stories_by_vote(hn):
+    return sorted(hn, key = lambda k:k['votes'],reverse=True)
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points',''))
+            if points > 0:
+                hn.append({
+                    'title' : title,
+                    'link' : href,
+                    'votes' : points
+                })
+    return hn
+
+pprint.pprint(create_custom_hn(links,subtext))
